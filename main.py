da8898abcc5c93689fcfd938bd25502230a7b399
Stas Masevych <109438310+StasMasevych@users.noreply.github.com>
2025-04-23 17:48:53 +0200

    add tasks for agents, basemodel class, time for agents work in paralel

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ warnings.filterwarnings('ignore')
 from crewai import Agent, Crew, Task
 
 import os
+import time
 from utils import get_openai_api_key,get_serper_api_key
 
 openai_api_key = get_openai_api_key()
@@ -13,9 +14,20 @@ os.environ["SERPER_API_KEY"] = get_serper_api_key()
 
 from crewai_tools import ScrapeWebsiteTool, SerperDevTool
 
+# Import Pydantic
+from pydantic import BaseModel
+
 # Initialize the tools
 search_tool = SerperDevTool()
 scrape_tool = ScrapeWebsiteTool()
+
+# Define a Pydantic model for venue details
+class VenueDetails(BaseModel):
+    name: str
+    address: str
+    capacity: int
+    booking_status: str
+
 
 # Create agents
 
@@ -40,7 +52,7 @@ logistics_manager = Agent(
     role='Logistics Manager',
     goal=(
         "Manage all logistics for the event "
-        "including catering and equipmen"
+        "including catering and equipment"
     ),
     tools=[search_tool, scrape_tool],
     verbose=True,
@@ -67,3 +79,73 @@ marketing_communications_agent = Agent(
     )
 )
 
+# Creating tasks
+
+venue_task = Task(
+    description="Find a venue in {event_city} "
+                "that meets criteria for {event_topic}.",
+    expected_output="All the details of a specifically chosen"
+                    "venue you found to accommodate the event.",
+    human_input=True,
+    output_json=VenueDetails,
+    output_file="venue_details.json",  
+      # Outputs the venue details as a JSON file
+    agent=venue_coordinator
+)
+
+logistics_task = Task(
+    description="Coordinate catering and "
+                 "equipment for an event "
+                 "with {expected_participants} participants "
+                 "on {tentative_date}.",
+    expected_output="Confirmation of all logistics arrangements "
+                    "including catering and equipment setup.",
+    human_input=True,
+    async_execution=True,
+    agent=logistics_manager
+)
+
+marketing_task = Task(
+    description="Promote the {event_topic} "
+                "aiming to engage at least"
+                "{expected_participants} potential attendees.",
+    expected_output="Report on marketing activities "
+                    "and attendee engagement formatted as markdown.",
+    async_execution=True,
+    output_file="marketing_report.md",  # Outputs the report as a text file
+    agent=marketing_communications_agent
+)
+
+# Create crew
+
+event_management_crew = Crew(
+    agents=[venue_coordinator, 
+            logistics_manager, 
+            marketing_communications_agent],
+    
+    tasks=[venue_task, 
+           logistics_task, 
+           marketing_task],
+    
+    verbose=True
+)
+
+event_details = {
+    'event_topic': "Tech Innovation Conference",
+    'event_description': "A gathering of tech innovators "
+                         "and industry leaders "
+                         "to explore future technologies.",
+    'event_city': "San Francisco",
+    'tentative_date': "2024-09-15",
+    'expected_participants': 500,
+    'budget': 20000,
+    'venue_type': "Conference Hall"
+}
+
+try:
+    result = event_management_crew.kickoff(inputs=event_details)
+finally:
+    # Allow time for threads to finish before Python shuts down
+    time.sleep(5)
+
+print(result)
